06186db0500ff1150fe8fc1dab4dc01bebd2a58e
nurkan <nurkan.dzhusupov@cbk.kg>
2022-07-28 11:20:42 +0600

    beta

--- src/main/java/kg/cbk/gateway/config/WebConfig.java
+++ src/main/java/kg/cbk/gateway/config/WebConfig.java
@@ -2,6 +2,7 @@ package kg.cbk.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -20,14 +21,13 @@ public class WebConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
 
-        http.csrf()
-                .disable()
-                .authorizeExchange()
-                .pathMatchers("/oauth/**", "/api/**")
-                .permitAll()
-                .and()
-                .oauth2ResourceServer()
-                .jwt();
+         http
+             .csrf()
+             .disable()
+                 .authorizeExchange()
+                 .pathMatchers("/oauth/**").permitAll()
+                 .pathMatchers("/acquire/**").permitAll()
+                 .pathMatchers("/qr/**").permitAll();
 
         return http.build();
     }
